82878ef01b39abd02aed46706863fe8c913ef220
warren <w.hammock@icloud.com>
2024-08-31 21:03:30 -0400

    Revert "inital commit"
    This reverts commit 6801639bb6004dc7b7d5014ec223ccd7311a5e74.

--- Python-for-AWS-with-Boto3/07-Lambda/trigger.py
+++
@@ -1,6 +0,0 @@
-import json
-
-def lambda_trigger(event, context):
-    s3_object_key = event['Records'][0]['s3']['object']['key']
-    print(f"File uploaded: {s3_object_key}")
-    return s3_object_key
